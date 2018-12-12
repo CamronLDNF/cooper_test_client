d925f4361a2921a9754b51db852d7a572c86444e
camron <camronmoradi@gmail.com>
2018-12-12 11:44:33 +0100

    Resolves test errors in home spec and person spec files

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,9 +1,11 @@
 import { HomePage } from "./home";
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, inject } from "@angular/core/testing";
 import { IonicModule, Platform, NavController } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+import { PersonProvider } from '../../providers/person/person';
+import { CooperProvider } from "../../providers/cooper/cooper";
 
 describe("HomePage", () => {
   let fixture, homepage;
@@ -18,7 +20,9 @@ describe("HomePage", () => {
         { provide: Platform, useFactory: () => PlatformMock.instance() },
         { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
         { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
-        { provide: NavController, useFactory: () => NavControllerMock.instance() }
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        PersonProvider,
+        CooperProvider
       ]
     }).compileComponents();
   }));
@@ -34,7 +38,7 @@ describe("HomePage", () => {
   });
 
   it('should have user default values', () => {
-    expect(homepage.user).toEqual({ distance: 1000, age: 20 });
+    expect(homepage.user).toEqual({ distance: 1000, age: 20, gender: 'female' });
   });
 
   // do we still need the below function if replicated further down?
